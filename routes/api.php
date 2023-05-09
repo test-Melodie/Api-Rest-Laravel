2dad09404891aedf40f4faab7c8c95b098dd708b
test-Melodie <melodiebellot@gmail.com>
2023-05-09 13:44:00 +0200

    Configuration et Setup

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProductsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,17 +9,11 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::get('products', [ProductsController::class,'index'])->name('products.index');
-Route::get('products/{product}', [ProductsController::class, 'show'])->name('products.show');
-Route::post('products', [ProductsController::class, 'store'])->name('products.store');
-Route::put('products/{product}', [ProductsController::class, 'update'])->name('products.update');
-Route::delete('products/{product}', [ProductsController::class, 'destroy'])->name('products.destroy');
