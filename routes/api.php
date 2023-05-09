67f51496a016207555d3a530cb419ae279487637
test-Melodie <melodiebellot@gmail.com>
2023-05-09 15:05:42 +0200

    Groupement points et terminaisons

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\UserAuthenticationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,10 +15,12 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->group(function(){
+    Route::post('login', [UserAuthenticationController::class, 'login']);
+    Route::post('register', [UserAuthenticationController::class, 'register']);
 });
-Route::post('login', [UserAuthenticationController::class, 'login']);
-Route::post('register', [UserAuthenticationController::class, 'register']);
-Route::post('logout', [UserAuthenticationController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::prefix('v1')->middleware('auth:sanctum')->group(function (){
+    Route::post('logout', [UserAuthenticationController::class, 'logout']);
+    Route::apiResource('products', ProductsController::class);
+});
