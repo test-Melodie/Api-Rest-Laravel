0e3d386574ab78147c512ae020d55e82429cdb4f
test-Melodie <melodiebellot@gmail.com>
2023-05-09 11:15:06 +0200

    Routes and endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('products', [ProductsController::class,'index'])->name('products.index');
+Route::get('products/{product}', [ProductsController::class, 'show'])->name('products.show');
+Route::post('products', [ProductsController::class, 'store'])->name('products.store');
+Route::put('products/{product}', [ProductsController::class, 'update'])->name('products.update');
+Route::delete('products/{product}', [ProductsController::class, 'destroy'])->name('products.destroy');
